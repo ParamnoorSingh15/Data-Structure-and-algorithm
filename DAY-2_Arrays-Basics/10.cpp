a3302595a4841e0424404e6a467be5469c2bc411
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-28 21:05:00 +0530

    Check if two arrays are identical (same elements, same order)

---
+++ DAY-2_Arrays-Basics/10.cpp
@@ -0,0 +1,44 @@
+// Check if two arrays are identical (same elements, same order)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n1;
+    int n2;
+    cin >> n1;
+    cin >> n2;
+    vector<int> arr1(n1);
+    vector<int> arr2(n2);
+
+    for(int i = 0; i<n1; i++){
+        cin >> arr1[i];
+    }
+
+    for(int i = 0; i<n1; i++){
+        cin >> arr2[i];
+    }
+
+    bool flag = true;
+
+    if(n1 != n2){
+        cout << "Unidentical" << endl;
+        return 0;
+    }
+
+
+    for(int i = 0; i<n1; i++){
+        if(arr1[i] != arr2[i]){
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag){
+        cout << "Identical" << endl;
+    }else{
+        cout << "Unidentical" << endl;
+    }
+
+
+}
