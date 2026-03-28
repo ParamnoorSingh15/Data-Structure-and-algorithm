d675e9a9f5da907b230538e49fa2a07916c5ce2d
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-28 20:38:15 +0530

    Check if array is sorted in ascending order

---
+++ DAY-2_Arrays-Basics/7.cpp
@@ -0,0 +1,30 @@
+// Check if array is sorted in ascending order
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    bool flag = true;
+    for(int i = 0; i<n-1; i++){
+        if(arr[i] > arr[i+1]){
+            flag = false;
+            break;
+        }
+    }
+    if(flag){
+        cout << "Array is sorted" << endl;
+    }else{
+        cout << "Array is not sorted" << endl;
+    }
+
+
+    return 0;
+}
