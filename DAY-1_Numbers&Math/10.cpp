269497a1d42e8e0e39dfc1ab0f21ffb7d5fae886
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-28 15:25:59 +0530

    Find which century a year belongs to (2001 -> 21st)

---
+++ DAY-1_Numbers&Math/10.cpp
@@ -0,0 +1,21 @@
+// Find which century a year belongs to (2001 -> 21st)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+    int century;
+
+    if(n % 100 == 0 ){
+        century = n/100;
+    }else{
+        century = n/100 + 1;
+    }
+    cout<<century<<endl;
+
+
+
+    return 0;
+}
