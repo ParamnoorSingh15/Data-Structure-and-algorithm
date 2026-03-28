405c51f20cc330b125b25a902c9569cc19f94683
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-28 20:14:53 +0530

    Linear search — return index, -1 if not found

---
+++ DAY-2_Arrays-Basics/5.cpp
@@ -0,0 +1,37 @@
+// Linear search — return index, -1 if not found
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int target;
+    cin >> target;
+
+    int index = -1;
+
+    for(int i = 0; i<n; i++){
+        if(arr[i] == target){
+            index = i;
+            break;
+        }
+    }
+
+    cout << index << endl;
+
+
+
+
+
+
+
+    return 0;
+}
