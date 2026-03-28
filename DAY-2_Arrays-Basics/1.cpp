13ce98f9d9209b53fcc29ebec2e1c39083b9f884
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-28 19:51:47 +0530

    Find minimum element in array

---
+++ DAY-2_Arrays-Basics/1.cpp
@@ -0,0 +1,29 @@
+// Find minimum element in array
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int min = arr[0];
+
+    for(int i = 1; i<n; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+
+    cout << min << endl;
+
+
+
+    return 0;
+}
