205c8bf06540e967dc0d285c98448c7fbad3b651
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-28 20:46:57 +0530

    Move all zeros to end while keeping relative order of non-zeros

---
+++ DAY-2_Arrays-Basics/8.cpp
@@ -0,0 +1,36 @@
+// Move all zeros to end while keeping relative order of non-zeros
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int pos = 0;
+
+    for(int i = 0; i<n; i++){
+        if(arr[i] != 0){
+            arr[pos++] = arr[i];
+        }
+    }
+
+    while(pos < n){
+        arr[pos++] = 0;
+    }
+
+    for(int i = 0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+
+
+    return 0;
+}
