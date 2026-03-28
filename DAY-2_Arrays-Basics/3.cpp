25be2b527a17622787199f5a763f614b152be619
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-28 20:03:28 +0530

    Find second largest element — single pass, NO sorting allowed

---
+++ DAY-2_Arrays-Basics/3.cpp
@@ -0,0 +1,44 @@
+// Find second largest element — single pass, NO sorting allowed
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector <int> arr(n);
+
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    int secmax = INT_MIN;
+
+    for(int i = 1; i<n; i++){
+        if(arr[i] > max && secmax < max){
+            max = arr[i];
+            secmax = max;
+        }
+    }
+
+    if(secmax == INT_MIN){
+        cout << -1 << endl;
+    }else{
+        cout << secmax << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
