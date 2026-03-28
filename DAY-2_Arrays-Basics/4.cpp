e6dc0e59c24127241feaba5416f01bc47e885f4b
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-28 20:09:45 +0530

    Reverse array in-place using two pointers

---
+++ DAY-2_Arrays-Basics/4.cpp
@@ -0,0 +1,29 @@
+// Reverse array in-place using two pointers
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int s = 0;
+    int e = n - 1;
+
+    while(s < e){
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
+    }
+
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+}
