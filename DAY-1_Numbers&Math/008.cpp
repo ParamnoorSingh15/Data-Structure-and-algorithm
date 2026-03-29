6b1bcd8770bea9107c9383c33c7f625e1872d264
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-29 16:59:39 +0530

    Count number of set bits (1s) in binary representation of N

---
+++ DAY-1_Numbers&Math/008.cpp
@@ -0,0 +1,15 @@
+// Count number of set bits (1s) in binary representation of N
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int count = 0;
+    while (n > 0) {
+        count += n & 1;
+        n >>= 1;
+    }
+    cout << count << endl;
+    return 0;
+}
