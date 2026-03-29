ec192b884f598418d64a8dbc62794cbd2825419f
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-29 18:51:14 +0530

    Count subarrays with sum equal to zero/K

--- DAY-3_Arrays-Intermediate/012.cpp
+++ DAY-3_Arrays-Intermediate/012.cpp
@@ -19,13 +19,16 @@ int main()
 
     // subArrSum[i,j] = PS[j] - PS[i-1] == k or 0
 
+    // CASE - 1 ->  PS[i-1] = PS[j] - k eg prefixsum -k  we have to find its occurence also count = count + occurence of PS[i-1]
+    // CASE - 2 -> if prefixSum == k then count ++;
+
     for(int i = 0; i<n; i++){
         prefixSum += arr[i];
         if(prefixSum == 0){
             count ++;
         }
 
-        if(mp.find(prefixSum) != mp.end()){
+        if(mp.find(prefixSum /* - K ie 0 here */) != mp.end()){
             count = count + mp[prefixSum];
         }
 
