fd097fca5b680f83c2abd6bb07c6af42fd3f7c3d
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-29 14:08:33 +0530

    Find all elements that appear odd number of times

---
+++ DAY-3_Arrays-Intermediate/4.cpp
@@ -0,0 +1,31 @@
+// Find all elements that appear odd number of times
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    unordered_map<int,int> mp;
+    for(int i = 0; i<n; i++){
+        mp[arr[i]] ++;
+    }
+
+    for(auto it : mp){
+        if(it.second % 2 != 0){
+            cout << it.first<<" ";
+        }
+    }
+    cout << endl;
+
+
+    return 0;
+
+}
