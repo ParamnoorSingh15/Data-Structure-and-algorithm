e5b9cf46d6e073505a26198236b3c0813933d4ac
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-29 16:54:17 +0530

    GCD of a number

---
+++ DAY-1_Numbers&Math/003.cpp
@@ -0,0 +1,27 @@
+// GCD of a number
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a;
+    cin >> b;
+    int gcd = 1;
+
+    // euclidean formula gcd(a,b) = gcd(b, a-b) if b becomes zero other one is gcd
+
+    while(a > 0 && b > 0){
+        if(a>b)
+            a = a%b;
+        else
+            b = b%a;
+        
+    }
+    if(a==0)
+        gcd = b;
+    else
+        gcd = a;
+    
+
+    cout << gcd << endl;
+}
