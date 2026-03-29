2042406713d755265cebafc15b3dc83bc418ff99
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-29 16:59:23 +0530

    Convert binary string to decimal number

---
+++ DAY-1_Numbers&Math/007.cpp
@@ -0,0 +1,20 @@
+// Convert binary string to decimal number
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; 
+    cin >> n;
+    int i = 1;
+
+    int ans = 0;
+
+    while(n > 0){
+        int bit = n%10;
+        ans  = ans + bit * i;
+        n = n / 10;
+        i = i*2;
+    }
+
+    cout << ans << endl;
+}
