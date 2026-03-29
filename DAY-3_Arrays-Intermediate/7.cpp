533cba49c5a99471ff45120c06479ac0c64ac1e6
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-03-29 16:01:53 +0530

    // Find if any duplicate exists in array (use HashSet)

---
+++ DAY-3_Arrays-Intermediate/7.cpp
@@ -0,0 +1,32 @@
+// Find if any duplicate exists in array (use HashSet)
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    unordered_set<int> st;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (st.find(arr[i]) != st.end())
+        {
+            cout << "YES" << endl;
+            return 0;
+        }
+        st.insert(arr[i]);
+    }
+
+    cout << "NO" << endl;
+
+    return 0;
+}
