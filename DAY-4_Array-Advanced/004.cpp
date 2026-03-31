cafe343c1824981d79d0170110e39033c26e5c04
ParamnoorSingh15 <paramnoor231@gmail.com>
2026-04-01 00:36:58 +0530

    Find last occurrence of X in sorted array — O(log n)

---
+++ DAY-4_Array-Advanced/004.cpp
@@ -0,0 +1,40 @@
+// Find last occurrence of X in sorted array — O(log n)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int x;
+    cin >> x;
+
+    int st = 0; 
+    int end = n-1;
+
+
+    int ans = -1;
+    while(st <= end){
+        int mid = st + (end-st)/2;
+        if(x > arr[mid])
+            st = mid + 1;
+        else if (x < arr[mid])
+            end = mid - 1;
+        else{
+            ans = mid;
+            st = mid + 1;
+        }
+    }
+    cout << ans << endl;
+        
+
+
+    return 0;
+}
